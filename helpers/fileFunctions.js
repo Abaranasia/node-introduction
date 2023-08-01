a92ff6e10f744e0a9d94708a48ae751ebafab59b
Ran Kirlian <rankirlian@gmail.com>
2023-08-01 10:57:25 +0200

    Update fileFunctions for linter format and function renaming

--- helpers/fileFunctions.js
+++ helpers/fileFunctions.js
@@ -1,26 +1,28 @@
 require('colors');
 const fs = require('fs');
 
-const nWriteFile = async (base = 5, lim = 10) => {
-    console.log('----------------------------------------------------------'.yellow);
-    console.log(`Tabla de multiplicar del ${base} x ${lim} generada con JS & Node`.cyan);
-    console.log('----------------------------------------------------------'.yellow);
+const createFile = async (base = 5, limit = 10) => {
+  console.log('-------------------------------------------------------'.yellow);
+  console.log(
+    `Tabla de multiplicar del ${base} x ${limit} generada con JS & Node`.cyan
+  );
+  console.log('-------------------------------------------------------'.yellow);
 
-    const filename = `output/tabla-${base}x${lim}.txt`
-    let salida = '';
+  const filename = `output/tabla-${base}x${limit}.txt`;
+  let salida = '';
 
-    try {
-        for (i = 1; i <= lim; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`;
-        };
-
-        fs.writeFileSync(filename, salida);
-        console.log(salida);
-        return filename;
-    } catch (err) {
-        throw err;
+  try {
+    for (i = 1; i <= limit; i++) {
+      salida += `${base} x ${i} = ${base * i}\n`;
     }
-}
+
+    fs.writeFileSync(filename, salida);
+    console.log(salida);
+    return filename;
+  } catch (err) {
+    throw err;
+  }
+};
 module.exports = {
-    nWriteFile
-}
+  createFile,
+};
